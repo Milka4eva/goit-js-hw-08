929d3e1ada46dc55ea630c98ac84b3f3b5cbc860
Mila Petruk <mila.petruk@gmail.com>
2023-03-12 18:44:35 +0200

    tasks 1, 2 and 3
    Scripts for Gallery, Video and Feedback done.
    Installed libraries Simplelightbox, Vimeo Player and Lodash.Throttle.

---
+++ src/js/03-feedback.js
@@ -0,0 +1,36 @@
+import { throttle } from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    const savedObject = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(savedObject));
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+const load = key => {
+  try {
+    const serialState = localStorage.getItem(key);
+    return serialState === null ? undefined : JSON.parse(serialState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const dataStored = load(LOCALSTORAGE_KEY);
+if (dataStored) {
+  email.value = dataStored.email;
+  message.value = dataStored.message;
+}
